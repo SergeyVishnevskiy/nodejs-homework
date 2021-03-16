10b21ce01821ed44236c0b6a749fac8352a9869f
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-03-16 08:32:39 +0200

    api

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -47,7 +47,7 @@ router.post("/", async (req, res, next) => {
     return res.status(201).json({
       status: "success",
       code: 201,
-      deta: {
+      data: {
         contact,
       },
     });
